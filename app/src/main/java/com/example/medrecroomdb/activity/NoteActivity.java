063774f0b45c5fef142608796d1dff5e465caac8
Diego <diegonarva95@gmail.com>
2022-03-17 16:40:33 -0400

    note activity created

---
+++ app/src/main/java/com/example/medrecroomdb/activity/NoteActivity.java
@@ -0,0 +1,16 @@
+package com.example.medrecroomdb.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.medrecroomdb.R;
+
+public class NoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+    }
+}
